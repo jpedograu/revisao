a554de06578a612d1654caed1c9c51c82050c772
jpedograu <ultrasenha@gmail.com>
2024-03-14 09:08:11 -0300

    xg

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+<body>
+    <div class="container">
+        <div class="coluna">
+            <div class="linha">
+                <h1> cadraste-se</h1>
+                <form>
+                    <div class="mb-3">
+                        <label for="nome" class="form-label">nome completo</label>
+                        <input type="text" class="form-control" name="nome">
+
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email">
+
+                        <label for="endereco" class="form-label">Endereço</label>
+                        <input type="text" class="form-control" name="endereco">
+                    
+
+                        <label for="telefone" class="form-label">telefone</label>
+                        <input type="number" class="form-control" name="telefone">
+
+                        <label for="data" class="form-label">Data_nascimento</label>
+                        <input type="date" class="form-control" name="data">
+
+                        <label for="senha" class="form-label">senha</label>
+                        <input type="password" class="form-control" name="senha">
+                        <input type="submit" class="btn btn-success">
+                       
+
+                      
+                     
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <h1</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
